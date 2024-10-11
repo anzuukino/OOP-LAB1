1ce37afb9e1f143fc097dbe565b9daca99e1587a
anzuukino <songokult1@gmail.com>
2024-10-11 18:03:22 +0700

    update bai 8

--- bai8.cpp
+++ bai8.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-const double lai_suat_ngan_han = 0.03;
-const double lai_suat_dai_han = 0.46;
-const double lai_suat_khong_ky_han = 0.05;
+const double lai_suat_ngan_han = 1.2;
+const double lai_suat_dai_han = 2.1;
+const double lai_suat_khong_ky_han = 0.5;
 
 struct Date{
     int day;
@@ -119,7 +119,7 @@ void Nhap(SoTietKiem &stk){
             cout << "So tien gui phai lon hon 0 Vui long nhap lai.\n";
         }
     } while (stk.So_tien_gui <= 0);
-
+    cin.ignore();
 }
 
 void HienThiThongTin(SoTietKiem stk){
@@ -170,20 +170,10 @@ bool sosanhngay2stk(SoTietKiem a, SoTietKiem b){
     return 0;
 }
 
-// double TinhTienLai(SoTietKiem stk){
-//     double tienlai = 0;
-//     return tienlai;
-// }
-
-// double rutTien(SoTietKiem stk, double tienrut){
-//     if (tienrut > stk.So_tien_gui){
-//         cout << "So tien rut khong hop le! Vui long nhap lai.\n";
-//     }else{
-//         cout << "Ban da rut so tien: " << tienrut << endl;
-//         stk.So_tien_gui -= tienrut;
-//     }
-//     return tienrut;
-// }
+double TinhTienLai(SoTietKiem stk){
+    double tienlai = 0;
+    return tienlai;
+}
 
 void today(Date &date){
     time_t now = time(0);
@@ -193,53 +183,137 @@ void today(Date &date){
     date.year = 1900 + ltm->tm_year;
 }
 
-// void rutTienMain(SoTietKiem &stk, double tienrut){
-//     Date ngayruttien;
-//     today(ngayruttien);
-//     int thoigian = 12 * (ngayruttien.year - stk.Ngay_mo_so.year) + (ngayruttien.month - stk.Ngay_mo_so.month);
-//     if (thoigian < 6 && stk.Loai_tiet_kiem != "Khong thoi han"){
-//         cout << "Ban dang rut tien truoc han! Lãi suất được điều chỉnh xuống 0.5%/năm.\n";
-//     }
-//     int choice = 0, ok = 1;
-//     while(ok){
-//         cout << "1. Rut toan bo so tien." << endl;
-//         cout << "2. Rut mot phan so tien." << endl;
-//         cout << "3. Thoat." << endl;
-//         cout << "Lua chon cua ban: ";
-//         cin >> choice;
-//         switch (choice){
-//             case 1:
-//                 double tien_rut = rutTien(stk, stk.So_tien_gui);
-//                 cout << "Ban da rut toan bo so tien: " << tien_rut << endl;
-//                 stk.So_tien_gui = 0;
-//                 break;
-//             case 2:
-//                 double tien_rut;
-//                 cout << "Nhap so tien can rut: ";
-//                 cin >> tien_rut;
-//                 double max_tien_rut = rutTien(stk,stk.So_tien_gui);
-//                 cout << "So tien hien co: " << max_tien_rut << endl;
-//                 if (tien_rut > max_tien_rut){
-//                     cout << "Ban khong co du tien Vui long nhap lai.\n";
-//                 }else{
-//                     cout << "Ban da rut so tien: " << tien_rut << endl;
-//                     stk.So_tien_gui -= tien_rut;
-//                 }
-//                 break;
-//             case 3:
-//                 ok = 0;
-//                 break;
-//             default:
-//                 cout << "Lua chon khong hop le. Vui long nhap lai." << endl;
-//         }
-//     }
-// }
+long double calcTienLai(SoTietKiem stk, double tienrut, double rate){
+    Date ngayruttien;
+    today(ngayruttien);
+    int DayinYear = 365;
+    int thoigian =  (ngayruttien.year - stk.Ngay_mo_so.year) * DayinYear + 
+    (ngayruttien.month - stk.Ngay_mo_so.month) * 30 + 
+    (ngayruttien.day - stk.Ngay_mo_so.day);
+    long double tienlai = 0;
+    tienlai = tienrut * (rate/100) * (thoigian / double(365));
+    return tienlai;
+}
+
+void rutTienMain(SoTietKiem &stk, double tienrut){
+    Date ngayruttien;
+    today(ngayruttien);
+    int DayinYear = 365;
+    int thoigian =  (ngayruttien.year - stk.Ngay_mo_so.year) * DayinYear + 
+    (ngayruttien.month - stk.Ngay_mo_so.month) * 30 + 
+    (ngayruttien.day - stk.Ngay_mo_so.day);
+    double rate = stk.lai_suat;
+    if (thoigian < 180){
+        cout << "Ban dang rut tien truoc han! Lai suat se duoc dieu chinh xuong 0.5%/năm.\n";
+        rate = 0.5;
+    }
+    int choice = 0, ok = 1;
+    while(ok){
+        cout << "1. Rut toan bo so tien." << endl;
+        cout << "2. Rut mot phan so tien." << endl;
+        cout << "3. Thoat." << endl;
+        cout << "Lua chon cua ban: ";
+        cin >> choice;
+        double tien_rut, max_tien_rut;
+        switch (choice){
+            case 1:
+                tien_rut = calcTienLai(stk, stk.So_tien_gui,rate);
+                cout << "Ban da rut toan bo so tien: " << tien_rut << endl;
+                stk.So_tien_gui = 0;
+                break;
+            case 2:
+                cout << "Nhap so tien can rut: ";
+                cin >> tien_rut;
+                max_tien_rut = calcTienLai(stk,stk.So_tien_gui,rate);
+                cout << "So tien hien co: " << max_tien_rut << endl;
+                if (tien_rut > max_tien_rut){
+                    cout << "Ban khong co du tien Vui long nhap lai.\n";
+                }else{
+                    cout << "Ban da rut so tien: " << tien_rut << endl;
+                    stk.So_tien_gui -= tien_rut;
+                }
+                break;
+            case 3:
+                ok = 0;
+                break;
+            default:
+                cout << "Lua chon khong hop le. Vui long nhap lai." << endl;
+        }
+    }
+}
+
+bool sosanh2date(Date a, Date b){
+    if (a.year < b.year){
+        return 1;
+    }else if (a.year == b.year){
+        if (a.month < b.month){
+            return 1;
+        }else if (a.month == b.month){
+            if (a.day <= b.day){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
 
 bool sosanhtien2stk(SoTietKiem a, SoTietKiem b){
     if (a.So_tien_gui > b.So_tien_gui){
         return 1;
     }
     return 0;
+}
+
+void sortMain(SoTietKiem stk[], int n){
+    int choice = 0, ok = 1;
+    while(ok){
+        cout << "1. Sap xep danh sach theo so tien giam dan." << endl;
+        cout << "2. Sap xep danh sach theo ngay mo so tang dan." << endl;
+        cout << "3. Thoat." << endl;
+        cout << "Lua chon cua ban: ";
+        cin >> choice;
+        switch (choice){
+            case 1:
+                cout << "Danh sach sau khi sap xep theo so tien giam dan: " << endl;
+                sort(stk, stk + n, sosanhtien2stk);
+                for (int i = 0; i < n; i++){
+                    HienThiThongTin(stk[i]);
+                }
+                break;
+            case 2:
+                cout << "Danh sach sau khi sap xep theo ngay mo so tang dan: " << endl;
+                sort(stk, stk + n, sosanhngay2stk);
+                for (int i = 0; i < n; i++){
+                    HienThiThongTin(stk[i]);
+                }
+                break;
+            case 3:
+                ok = 0;
+                break;
+            default:
+                cout << "Lua chon khong hop le. Vui long nhap lai." << endl;
+        }
+    }
+}
+
+void LietKeMain(SoTietKiem stk[], int n){
+    Date ngaybatdau, ngayketthuc;
+    cout << "Nhap ngay bat dau (dd mm yyyy): ";
+    cin >> ngaybatdau.day >> ngaybatdau.month >> ngaybatdau.year;
+    cout << "Nhap ngay ket thuc (dd mm yyyy): ";
+    cin >> ngayketthuc.day >> ngayketthuc.month >> ngayketthuc.year;
+    for (int i = 0; i < n; i++){
+        if (sosanh2date(ngaybatdau, stk[i].Ngay_mo_so) && sosanh2date(stk[i].Ngay_mo_so, ngayketthuc)){
+            HienThiThongTin(stk[i]);
+        }
+    }
+}
+
+void capnhatLaisuat(SoTietKiem &stk, double laisuat){
+    stk.lai_suat = laisuat;
+    cout << "Da cap nhat lai suat cho so tiet kiem: " << stk.MaSo << endl;
+    long double tienlai = calcTienLai(stk, stk.So_tien_gui, stk.lai_suat);
+    cout << "So tien lai hien tai: " << tienlai << endl;
 }
 
 int main(){
@@ -252,6 +326,91 @@ int main(){
         Nhap(stk[i]);
     }
 
+    int choice = 0, ok = 1;
+    while(ok){
+        cout << "1. Tim kiem so tiet kiem theo CMND hoac ma so." << endl;
+        cout << "2. Liet ke cac so tiet kiem duoc mo trong mot khoang thoi gian." << endl;
+        cout << "3. Sap xep danh sach so tiet kiem." << endl;
+        cout << "4. Cap nhat lai suat." << endl;
+        cout << "5. Tinh tien lai hien tai." << endl;
+        cout << "6. Rut tien." << endl;
+        cout << "7. Thoat." << endl;
+        cout << "Lua chon cua ban: ";
+        cin >> choice;
+        string tukhoa;
+        string masotietkiem;
+        long double tienrut, laisuat;
+        switch (choice){
+            case 1:
+                cout << "Nhap thong tin cua so tiet kiem can tim: ";
+                cin.ignore();
+                getline(cin, tukhoa);
+                TimKiemTheoMaSo(stk, tukhoa, n);
+                break;
+            case 2:
+                LietKeMain(stk, n);
+                break;
+            case 3:
+                sortMain(stk, n);
+                break;
+            case 4:
+                cin.ignore();
+                do {
+                    cout << "Nhap ma so tiet kiem: ";
+                    getline(cin, masotietkiem);
+                    if (!isValidCode(masotietkiem)) {
+                        cout << "Ma so khong hop le! Vui long nhap lai.\n";
+                    }
+                }while(!isValidCode(masotietkiem));
+                cin >> laisuat;
+                for (int i = 0; i < n; i++){
+                    if (stk[i].MaSo == masotietkiem){
+                        capnhatLaisuat(stk[i], laisuat);
+                        break;
+                    }
+                }
+                break;
+            case 5:
+                cin.ignore();
+                do {
+                    cout << "Nhap ma so tiet kiem: ";
+                    getline(cin, masotietkiem);
+                    if (!isValidCode(masotietkiem)) {
+                        cout << "Ma so khong hop le! Vui long nhap lai.\n";
+                    }
+                }while(!isValidCode(masotietkiem));
+                for (int i = 0; i < n; i++){
+                    if (stk[i].MaSo == masotietkiem){
+                        long double tienlai = calcTienLai(stk[i], stk[i].So_tien_gui, stk[i].lai_suat);
+                        cout << "So tien lai hien tai: " << tienlai << endl;
+                        break;
+                    }
+                }
+                break;
+            case 6:
+                cin.ignore();
+                do {
+                    cout << "Nhap ma so tiet kiem: ";
+                    getline(cin, masotietkiem);
+                    if (!isValidCode(masotietkiem)) {
+                        cout << "Ma so khong hop le! Vui long nhap lai.\n";
+                    }
+                }while(!isValidCode(masotietkiem));
+                cin >> tienrut;
+                for (int i = 0; i < n; i++){
+                    if (stk[i].MaSo == masotietkiem){
+                        rutTienMain(stk[i], tienrut);
+                        break;
+                    }
+                }
+                break;
+            case 7:
+                ok = 0;
+                break;
+            default:
+                cout << "Lua chon khong hop le. Vui long nhap lai." << endl;
+        }
+    }
     
 
     return 0;
